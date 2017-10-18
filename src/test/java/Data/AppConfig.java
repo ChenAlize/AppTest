fafe6b0a028f25131bc97c7470b7c8b4848c06f5
e <2282440078@qq.com>
2017-10-19 00:52:04 +0800

    ---- readXml

--- src/test/java/Data/AppConfig.java
+++ src/test/java/Data/AppConfig.java
@@ -1,5 +1,6 @@
 package Data;
 
+import Util.Log;
 import Util.ReadXml;
 import org.testng.annotations.Test;
 
@@ -7,39 +8,52 @@ import org.testng.annotations.Test;
  * Created by chenbo on 2017/10/18.
  */
 public class AppConfig {
+    Log log = new Log ( this.getClass () );
     private String appValue;         //APP版本
     private String appPackage;       //APP包名
     private String appActivity;      //APP的Activity
-    private String cellphone;        //手机型号
+    private String cellphone = "HAWEI mate 9";        //手机型号
     private String platformVersion;  //安卓系统版本
     private String udid;             //设备号
     private int isLogin = 0 ;        //是否已登录
 
     public AppConfig(){
-
     }
 
-    public AppConfig( String appValue , String appPackage , String appActivity , String cellphone ,String platformVersion ,String udid){
-        this.appValue = appValue;
-        this.appPackage = appPackage;
-        this.appActivity = appActivity;
-        this.cellphone = cellphone;
-        this.platformVersion = platformVersion ;
-        this.udid = udid;
+    public AppConfig( String Value , String Package , String Activity ,String Version ,String DevicesID ){
+        this.appValue = Value;
+        this.appPackage = Package;
+        this.appActivity = Activity;
+        this.platformVersion = Version ;
+        this.udid = DevicesID;
+        log.info( appValue );
+        log.info( appPackage );
+        log.info( appActivity );
+        log.info( platformVersion );
+        log.info( udid );
     }
 
     public String getAppValue(){
         return this.appValue;
     }
-
-    public void xml(){
-        ReadXml readXml = new ReadXml ();
-        readXml.readXml ( "Android" , this.getClass ().getSimpleName ());
+    public String getAppPackage(){
+        return this.appPackage;
     }
+    public String getAppActivity(){
+        return this.appActivity;
+    }
+    public String getplatformVersion(){
+        return this.platformVersion;
+    }
+    public String getudid(){
+        return this.udid;
+    }
+
+
 
     @Test(enabled = true )
     public  void test(){
-        AppConfig appConfig = new AppConfig (  );
-        appConfig.xml ();
+        ReadXml read = new ReadXml ();
+        read.readXml ( cellphone  );
     }
 }
